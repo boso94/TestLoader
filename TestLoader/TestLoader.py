18ca7c863de9083523ac0092fe633a9a753daa66
Boso94 <thomas.agostinelli@gmail.com>
2022-04-28 00:38:51 +0200

    hh

--- TestLoader/TestLoader.py
+++ TestLoader/TestLoader.py
@@ -1,31 +1,55 @@
 import json
 from os import path
+import datetime 
 
+#date = datetime.datetime.now()
+#date = date.strftime("%d/%m/%Y")
 filename = 'c:/users/thoma/source/repos/testloader/testloader/test_results.json'
 listObj = []
 
 if path.isfile(filename) is False:
   raise Exception("File not found")
 
-name = input("Inserire nome utente: ")
-no_test = input("Inserire il numero del test: ")
-device = input("Inserire il device usato dall'utente: ")
-time = input("Inserire il tempo impiegato per sostenere il test: ")
+name = 'nome_' + input("Inserire nome utente: ")
+no_test = 'no_test_' + input("Inserire il numero del test: ")
+while True:
+    device_ans = input("Inserire il device usato dall'utente (Ar, Hololens, Manuale): ")
+    if device_ans in ['a', 'h', 'm']:
+        break
+if device_ans == 'a':
+    device = 'AR'
+if device_ans == 'h':
+    device = 'hololens'
+if device_ans == 'm':
+    device = 'manuale'
+time = int(input("Inserire il tempo impiegato per sostenere il test: "))
 
 with open(filename, 'r') as fp:
-  listObj = json.loads(fp)
+  listObj = json.load(fp)
 
-listObj.append({
-        'name': name,
-        'no_test': no_test,
-        'device': device,
-        'time': time
-})
+listObj.append(
+    {'28042022': 
+        [
+            {
+            name: 
+                [
+                    {
+                    no_test: 
+                           [
+                               {
+                                'device': device,
+                                'time': time
+                                }
+                           ]
+                    }
+                ]
+            }
+        ]
+     }
+)
 
 with open(filename, 'w') as json_file:
-    json.dumps(listObj, json_file, 
-                        indent=4,  
-                        separators=(',',': '))
+    json.dump(listObj, json_file)
 
 
 
